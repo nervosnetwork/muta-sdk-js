30c18a5ae4d547573e66aef1ed115da33c9230fe
homura <homura.dev@gmail.com>
2019-10-21 22:41:24 +0800

    feat(utils): support transform buffer and hex

---
+++ src/utils.ts
@@ -0,0 +1,19 @@
+export function rm0x(hex: string) {
+  return hex.startsWith('0x') ? hex.slice(2) : hex;
+}
+
+export function toHex(x: Buffer | number | string): string {
+  if (typeof x === 'string') {
+    if (x.startsWith('0x')) return x;
+    return '0x' + x;
+  }
+  if (typeof x === 'number') {
+    const hex = Number(x).toString(16);
+    return hex.length % 2 === 1 ? '0x0' + hex : '0x' + hex;
+  }
+  return '0x' + x.toString('hex');
+}
+
+export function toBuffer(x: string): Buffer {
+  return Buffer.from(rm0x(x), 'hex');
+}
