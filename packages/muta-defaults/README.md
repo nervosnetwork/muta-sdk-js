c8df3195c2b9dab51d887b734c7b0a1eb7ca7b95
homura <homura.dev@gmail.com>
2020-06-10 14:52:33 +0800

    docs(defaults): add docs for default module

---
+++ packages/muta-defaults/README.md
@@ -0,0 +1,34 @@
+# @mutajs/defaults
+
+Provide the default configuration or read configuration from environment variables for SDK instance
+
+## Environment Variables
+
+### MUTA_ENDPOINT
+
+The Muta GraphQL endpoint
+
+- type: string
+- default: http://127.0.0.1:8000/graphql
+
+### MUTA_CHAIN_ID
+
+A hex formated string for chain_id
+
+- type: string
+- default: 0xb6a4d7da21443f5e816e8700eea87610e6d769657d6b8ec73028457bf2ca4036
+
+### MUTA_CONSENSUS_INTERVAL
+
+Interval (in ms) of consensus engine executed each block.
+
+- type: number
+- default: 3000
+
+### MUTA_TIMEOUT_GAP
+
+A number for max available unprocessed block height gap
+
+- type: number
+- default: 20
+
--- packages/muta-defaults/src/defaults.ts
+++ packages/muta-defaults/src/defaults.ts
@@ -7,12 +7,13 @@ function envNum(key: string, defaults: number): number {
 }
 
 /**
- * the default TIMEOUT_GAP
+ * The default TIMEOUT_GAP. Lookup for `MUTA_TIMEOUT_GAP` in env or defaults to 20
  */
 export const DEFAULT_TIMEOUT_GAP = envNum('MUTA_TIMEOUT_GAP', 20);
 
 /**
- * The default chain id
+ * The default chain id. Lookup for `MUTA_CHAIN_ID` in env or
+ * defaults to `0xb6a4d7da21443f5e816e8700eea87610e6d769657d6b8ec73028457bf2ca4036`
  */
 export const DEFAULT_CHAIN_ID = envStr(
   'MUTA_CHAIN_ID',
@@ -20,7 +21,8 @@ export const DEFAULT_CHAIN_ID = envStr(
 );
 
 /**
- * Interval of consensus engine executed each block
+ * Interval(in ms) of consensus engine executed each block. Look up for `MUTA_CONSENSUS_INTERVAL` in env or
+ * defaults to _3000_
  */
 export const DEFAULT_CONSENSUS_INTERVAL = envNum(
   'MUTA_CONSENSUS_INTERVAL',
@@ -30,7 +32,8 @@ export const DEFAULT_CONSENSUS_INTERVAL = envNum(
 /**
  * The Muta project is now in the early stage,
  * so the default endpoint will use localhost.
- * This value will likely change after test network mounted
+ * This value will likely change after test network mounted.
+ * Lookup for `MUTA_ENDPOINT` in env or defaults to `http://127.0.0.1:8000/graphql`
  */
 export const DEFAULT_ENDPOINT = envStr(
   'MUTA_ENDPOINT',
