ff3f385a3e4b9ad0ce3de9871f8d9d1cd790a00e
wancencen <wancencen@cryptape.com>
2020-07-10 16:05:27 +0800

    feat: supported `AuthorizationService`, `MultiSignatureService`

---
+++ packages/muta-service/src/builtins/AuthorizationService.ts
@@ -0,0 +1,28 @@
+import { Address } from '@mutadev/types';
+   import { createServiceBindingClass, read, write } from '../create';
+
+   interface AddVerifiedItemPayload {
+     service_name: string;
+     method_name: string;
+   }
+
+   interface RemoveVerifiedItemPayload {
+     service_name: string;
+     method_name: string;
+   }
+
+   interface SetAdminPayload {
+     new_admin: Address;
+   }
+
+   export const AuthorizationService = createServiceBindingClass({
+     serviceName: 'authorization',
+     read: {
+       check_authorization: read<string, null>(),
+     },
+     write: {
+       add_verified_item: write<AddVerifiedItemPayload, null>(),
+       remove_verified_item: write<RemoveVerifiedItemPayload, null>(),
+       set_admin: write<SetAdminPayload, null>(),
+     },
+   });
--- packages/muta-service/src/index.ts
+++ packages/muta-service/src/index.ts
@@ -1,4 +1,6 @@
 export * from './create';
 
 export { AssetService } from './builtins/AssetService';
+export { AuthorizationService } from './builtins/AuthorizationService';
 export { MetadataService } from './builtins/MetadataService';
+export { MultiSignatureService } from './builtins/MultiSignatureService';
