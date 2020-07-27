fff2a3e6b49930e1fc42ea881c8a5d7a2349e2fc
homura <homura.dev@gmail.com>
2020-07-27 12:14:11 +0800

    chore: reduce jest config

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: 'packages',
-  testMatch: ['<rootDir>/**/*.test.ts'],
+  testMatch: ['<rootDir>/**/*.(test|e2e).ts'],
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.test.json'
     }
-  }
+  },
 };
