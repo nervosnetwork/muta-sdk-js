6e67089ebece5995686e71f325eac74096ea5497
homura <homura.dev@gmail.com>
2020-07-29 18:05:56 +0800

    chore:  example for how to new client

--- examples/001-get-some-block-info.js
+++ examples/001-get-some-block-info.js
@@ -1,9 +1,25 @@
-const { Client } = require('@mutadev/muta-sdk');
+const { Client, utils } = require('@mutadev/muta-sdk');
 
 async function main() {
-  const client = new Client({
-    endpoint: 'http://localhost:8000/graphql',
-  });
+  // const client = new Client({
+  //   endpoint: 'http://localhost:8000/graphql',
+  //   account: new Account(
+  //     '0x0000000000000000000000000000000000000000000000000000000000000001',
+  //   ),
+  //   chainId:
+  //     '0xb6a4d7da21443f5e816e8700eea87610e6d769657d6b8ec73028457bf2ca4036',
+  //   consensusInterval: 3000,
+  //   defaultCyclesLimit: '0xffffffff',
+  //   defaultCyclesPrice: '0xffffffff',
+  //   maxTimeout: 60000,
+  //   timeoutGap: 20,
+  //   serializer: {
+  //     deserialize: utils.safeParseJSON,
+  //     serialize: utils.safeStringifyJSON,
+  //   },
+  // });
+
+  const client = new Client();
 
   const block = await client.getBlock();
   console.log(`current height(hex): ${block.header.height}`);
